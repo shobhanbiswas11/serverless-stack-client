7b357c93fd4cb8515369ee99fa72ac120eefc1a3
Shobhan Biswas <shobhan@Shobhans-iMac.local>
2020-03-10 07:39:26 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-south-1",
+    BUCKET: "shobhan-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://02kr621uz0.execute-api.ap-south-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_ruupQm1kx",
+    APP_CLIENT_ID: "4b4gn35d2dmktm50fh6mdnfvhr",
+    IDENTITY_POOL_ID: "ap-south-1:00b69030-b5b3-41a8-8b41-c5e6103829cb"
+  }
+};
