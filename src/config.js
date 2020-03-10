7e23461162adf554a4f75a2f6e01d574fa2c6c4a
Shobhan Biswas <shobhan@Shobhans-iMac.local>
2020-03-10 20:54:03 +0530

    Building our React app

--- src/config.js
+++ src/config.js
@@ -12,5 +12,7 @@ export default {
     USER_POOL_ID: "ap-south-1_ruupQm1kx",
     APP_CLIENT_ID: "4b4gn35d2dmktm50fh6mdnfvhr",
     IDENTITY_POOL_ID: "ap-south-1:00b69030-b5b3-41a8-8b41-c5e6103829cb"
-  }
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_8JyGasAGaYlyGgirlU5evj9K00UtxdcEHW"
 };
